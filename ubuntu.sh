6c343f2bf3a5b8870e4f701ce458dce458445b52
Flávio Visetti <flavio.visetti@gmail.com>
2025-03-26 13:34:53 -0300

    improve ubuntu setup

--- ubuntu.sh
+++ ubuntu.sh
@@ -3,12 +3,9 @@
 set -e
 set -o pipefail
 
-NERD_FONT_VERSION='v3.0.2'
-CUSTOM_FONT_URL="https://github.com/ryanoasis/nerd-fonts/releases/download/${NERD_FONT_VERSION}/Meslo.zip"
-CUSTOM_FONT_NAME='Meslo.zip'
-
-NEOVIM_SOURCE_URL='https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz'
-NEOVIM_FILE_NAME='nvim-linux64'
+NERD_FONT_VERSION='v3.3.0'
+CUSTOM_FONT_NAME='JetBrainsMono'
+CUSTOM_FONT_URL="https://github.com/ryanoasis/nerd-fonts/releases/download/${NERD_FONT_VERSION}/${CUSTOM_FONT_NAME}.zip"
 
 ASDF_LATEST_VERSION='v0.16.6'
 ASDF_DOWNLOAD_URL="https://github.com/asdf-vm/asdf/releases/download/${ASDF_LATEST_VERSION}/asdf-${ASDF_LATEST_VERSION}-linux-amd64.tar.gz"
@@ -32,6 +29,7 @@ REQUIREMENTS_DEBIAN_PACK=(
   libz-dev
   libyaml-dev
   libssl-dev
+  neovim
 )
 
 install_nala() {
@@ -49,19 +47,17 @@ download_and_install_custom_font() {
     mkdir -p ${HOME}/.local/share/fonts
   fi
 
-  command wget $CUSTOM_FONT_URL -O "${DOTFILES_DEFAULT_PATH}/fonts/${CUSTOM_FONT_NAME}"
-  command unzip "${DOTFILES_DEFAULT_PATH}/fonts/${CUSTOM_FONT_NAME}" -d ${HOME}/.local/share/fonts
+  command wget $CUSTOM_FONT_URL -O "${DOTFILES_DEFAULT_PATH}/fonts/${CUSTOM_FONT_NAME}.zip"
+  command unzip "${DOTFILES_DEFAULT_PATH}/fonts/${CUSTOM_FONT_NAME}.zip" -d ${HOME}/.local/share/fonts
   command fc-cache -fv
 }
 
-download_and_install_neovim() {
-  command curl -L ${NEOVIM_SOURCE_URL} --output ${APPLICATION_PATH}/${NEOVIM_FILE_NAME}.tar.gz
-  command tar -xvzf ${APPLICATION_PATH}/${NEOVIM_FILE_NAME}.tar.gz --directory ${APPLICATION_PATH}
-  command ln -nfs ${APPLICATION_PATH}/${NEOVIM_FILE_NAME}/bin/nvim ${HOME}/.bin/nvim
-  command rm -r ${APPLICATION_PATH}/${NEOVIM_FILE_NAME}.tar.gz
-}
-
 download_and_install_asdf() {
+  if [ ! -d "${HOME}/.local/share/fonts" ]; then
+    echo "ASDF directory not found in $USER home. Creating directory..."
+    mkdir -p ${HOME}/.asdf
+  fi
+
   command curl -L ${ASDF_LATEST_VERSION} --output ${HOME}/${ASDF_FILE_NAME}.tar.gz
   command tar -xvzf ${HOME}/${ASDF_FILE_NAME}.tar.gz --directory ${HOME}/.asdf
 }
@@ -69,5 +65,4 @@ download_and_install_asdf() {
 install_nala
 install_debian_requirements
 download_and_install_custom_font
-download_and_install_neovim
 download_and_install_asdf
